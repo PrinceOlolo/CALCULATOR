703f24bdc352e77d9eab403484b9b14670073ba3
Sheog <Pr.nceOlolo@gmail.com>
2024-07-30 18:17:30 +0300

    1.01

--- src/main/java/me/prince/ololo/calculator/dto/CreditDataRq.java
+++ src/main/java/me/prince/ololo/calculator/dto/CreditDataRq.java
@@ -1,17 +1,22 @@
 package me.prince.ololo.calculator.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.math.BigDecimal;
 
 public class CreditDataRq {
-
+    @JsonProperty("principal")
     private final BigDecimal principal;
+    @JsonProperty("term")
     private final Integer term;
+    @JsonProperty("interest")
     private final BigDecimal interest;
 
     public CreditDataRq(BigDecimal principal, Integer term, BigDecimal interest) {
         this.principal = principal; //сумма
         this.term = term; //срок
         this.interest = interest; //проценты
+        System.out.println(principal + " " + term + " " + interest);
     }
 
     public BigDecimal getPrincipal() {
--- src/main/java/me/prince/ololo/calculator/service/CreditService.java
+++ src/main/java/me/prince/ololo/calculator/service/CreditService.java
@@ -13,11 +13,10 @@ public class CreditService {
         BigDecimal paymentAmount;
         BigDecimal monthlyRate;
         BigDecimal annuityRate;
-        Integer term = rq.getTerm();
 
         monthlyRate = rq.getInterest().divide(BigDecimal.valueOf(12), 10, RoundingMode.UP).divide(BigDecimal.valueOf(100), 10, RoundingMode.UP);
 
-        BigDecimal intermediate = BigDecimal.ONE.add(monthlyRate).pow(term);
+        BigDecimal intermediate = BigDecimal.ONE.add(monthlyRate).pow(rq.getTerm());
 
         annuityRate = monthlyRate.multiply(intermediate).divide(intermediate.subtract(BigDecimal.ONE),10, RoundingMode.UP);
 
