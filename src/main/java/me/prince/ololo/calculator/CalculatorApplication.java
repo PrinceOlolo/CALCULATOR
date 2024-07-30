9d071c929b9391095ed5f1333cebb7f9a532c0cc
Sheog <Pr.nceOlolo@gmail.com>
2024-07-30 16:26:01 +0300

    1.01

--- src/main/java/me/prince/ololo/calculator/CalculatorApplication.java
+++ src/main/java/me/prince/ololo/calculator/CalculatorApplication.java
@@ -12,12 +12,15 @@ public class CalculatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CalculatorApplication.class, args);
+
+       /*
         BigDecimal testPrincipal = new BigDecimal("100000");
         Integer testTerm = 24 ;
         BigDecimal testInterest = new BigDecimal("7.5");
         CreditDataRq rq = new CreditDataRq(testPrincipal, testTerm, testInterest);
         CreditService crs = new CreditService();
         System.out.println(crs.calculatePayment(rq).getPayment());
+        */
     }
 
 }
